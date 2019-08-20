3abdf24944d23439b2acebcaa910ad1f1cb1fd13
João Felipe <39709263+ja1felipe@users.noreply.github.com>
2019-08-19 21:00:21 -0300

    Trying deploy

--- server.js
+++ server.js
@@ -1,15 +1,13 @@
-//Install express server
 const express = require('express');
 const path = require('path');
- 
+const nomeApp = "elifeFront";
 const app = express();
  
-// Serve only the static files form the dist directory
-// Replace the '/dist/<to_your_project_name>'
-app.use(express.static(__dirname + '/dist/'));
-app.listen(process.env.PORT || 8080);
-app.get('*', function(req,res) {
-  // Replace the '/dist/<to_your_project_name>/index.html'
-  res.sendFile(path.join(__dirname +'/dist/index.html'));
+app.use(express.static(`${__dirname}/dist/${nomeApp}`));
+ 
+app.get('/*', (req, res) => {
+res.sendFile(path.join(`${__dirname}/dist/${nomeApp}/index.html`));
 });
-// Start the app by listening on the default Heroku port
+ 
+app.listen(process.env.PORT || 4200);
+console.log(`listening for ${nomeApp}`)
